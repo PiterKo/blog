5f3620f3dcb4ef7b1d2b4b1c0952f73cbc6eb10b
Piotr Kolodziejek <piotr.kolodziejek@outlook.com>
2018-11-24 22:12:46 +0100

    Adding posts and post component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,17 @@ import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
 import { CollapseModule } from 'ngx-bootstrap';
 import { SiteService } from './services/site.service';
+import { PostsComponent } from './posts/posts.component';
+import { PostComponent } from './post/post.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    FooterComponent
+    FooterComponent,
+    PostsComponent,
+    PostComponent
   ],
   imports: [
     BrowserModule,
